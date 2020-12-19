5c06d8fc5605234674e88474e1296560d92ab593
Thomas Gilbert <tcgilbert94@gmail.com>
2020-12-19 10:12:15 -0500

    stripped out login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,67 +1,75 @@
 // Imports
-import React, { useState } from 'react';
-import axios from 'axios';
-import jwt_decode from 'jwt-decode';
-import setAuthToken from '../utils/setAuthToken';
-import { Redirect } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "../utils/setAuthToken";
+import { Redirect } from "react-router-dom";
 const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
-
 const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
-    }
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const handlePassword = (e) => {
-        setPassword(e.target.value);
-    }
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const userData = { email, password };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userData = { email, password };
 
-        axios.post(`${REACT_APP_SERVER_URL}/api/users/login`, userData)
-        .then(response => {
-            const { token } = response.data;
-            // Save token to localStorage
-            localStorage.setItem('jwtToken', token);
-            // Set token to auth header
-            setAuthToken(token);
-            // Decode token to get the user data
-            const decoded = jwt_decode(token);
-            // Set current user
-            props.nowCurrentUser(decoded);
-        })
-        .catch(error =>{
-            console.log(error);
-        })
-    }
+    axios
+      .post(`${REACT_APP_SERVER_URL}/api/users/login`, userData)
+      .then((response) => {
+        const { token } = response.data;
+        // Save token to localStorage
+        localStorage.setItem("jwtToken", token);
+        // Set token to auth header
+        setAuthToken(token);
+        // Decode token to get the user data
+        const decoded = jwt_decode(token);
+        // Set current user
+        props.nowCurrentUser(decoded);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    if (props.user) return <Redirect to='/profile' />
+  if (props.user) return <Redirect to="/profile"/>;
 
-    return (
-        <div className="row mt-4">
-            <div className="col-md-7 offset-md-3">
-                <div className="card card-body">
-                    <h2 className="py-2">Login</h2>
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name="email" value={email} onChange={handleEmail} className="form-control" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" name="password" value={password} onChange={handlePassword} className="form-control" />
-                        </div>
-                        <button type="submit" className="btn btn-primary float-right">Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleEmail}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={handlePassword}
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
