560b93dde459cc3af4aa8fb688583dfe3e5dbda4
Thomas Gilbert <tcgilbert94@gmail.com>
2020-12-19 10:02:33 -0500

    stripped down nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,38 @@
 import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = (props) => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
-                <Link className="navbar-brand" to="/">MERN Auth</Link>
-
+        <nav>
                 <div>
                     <ul>
                         <li>
                             <NavLink exact to="/">Home</NavLink>
-                        </li>
-                        <li>
-                            <NavLink  to="/about">About</NavLink>
-                        </li>
-                    </ul>
+                        </li>          
                     {
                         props.isAuth 
-                        ? <ul>
+                        ? 
+                        <>
                             <li>
                                 <NavLink  to="/profile">Profile</NavLink>
                             </li>
                             <li>
                                 <span onClick={props.handleLogout}>Logout</span>
                             </li>
-                        </ul>
-                        : <ul>
+                        </>
+                        : 
+                        <>
                             <li>
                                 <NavLink to="/signup">Create Account</NavLink>
                             </li>
                             <li>
                                 <NavLink to="/login">Login</NavLink>
                             </li>
-                          </ul>
+                        </>
                     }
+                    </ul>
                 </div>
-            </div>
+            
         </nav>
     );
 }
